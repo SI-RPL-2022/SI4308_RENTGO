4bddd3439588da0245574f9e8311ae27bf5ba74b
gaurinanda06 <nanda.gauri06@gmail.com>
2022-05-10 23:03:49 +0700

    update gauri

---
+++ RentGo/resources/views/errors/404.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.auth')
+
+@section('main-content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <div class="row">
+                        <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                        <div class="col-lg-6">
+                            <div class="p-5">
+                                <div class="text-center mb-4">
+                                    <p class="text-title-login">404 | Not Found</p>
+                                    <img src="{{asset('img/il_title_login.png')}}" width="200" alt="">
+                                </div>
+
+                                @if ($errors->any())
+                                <div class="alert alert-danger border-left-danger" role="alert">
+                                    <ul class="pl-4 my-2">
+                                        @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                                @endif
+
+                                <div class="text-center mt-5">
+                                    <a class="button btn btn-danger" href="{{ route('home') }}">{{ __('back to landing page') }}</a>
+                                </div>
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
